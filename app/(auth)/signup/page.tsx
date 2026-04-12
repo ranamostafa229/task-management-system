9e7f38006b3cb77c8cd631e19f4f068fd83362b6
Your Name <ranamostafa2210@gmail.com>
2026-04-13 00:29:11 +0200

    feat(signup): refactor signup page to use separate SignupForm component

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,54 +7,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { signupSchema, SignUpSchemaType } from "@/lib/zodSchemas";
-import { zodResolver } from "@hookform/resolvers/zod";
+
 import Link from "next/link";
-import { Controller, useForm } from "react-hook-form";
-import { tryCatch } from "@/hooks/try-catch";
-import { toast } from "sonner";
-import { SignUpAction } from "./action";
-import { useTransition } from "react";
-import { Loader2 } from "lucide-react";
-import { useRouter } from "next/navigation";
+import SignupForm from "./_components/SignupForm";
 
 const SignUp = () => {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-  const { control, handleSubmit, reset } = useForm({
-    resolver: zodResolver(signupSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-      jobTitle: "",
-    },
-  });
-  const onSubmit = async (data: SignUpSchemaType) => {
-    startTransition(async () => {
-      const { data: result, error } = await tryCatch(SignUpAction(data));
-      if (error) {
-        console.error("Signup failed:", error);
-        toast.error("An error occurred during signup. Please try again.");
-      }
-      if (result?.status === "success") {
-        toast.success(result?.message || "Signup successful");
-        reset();
-        router.push("/");
-      } else {
-        toast.error(result?.message || "Failed to signup. Please try again.");
-      }
-    });
-  };
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <Card className="max-w-xl w-full mx-auto rounded-md py-12 px-12 mb-4 ">
@@ -68,155 +24,7 @@ const SignUp = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <FieldGroup>
-              <Controller
-                name="name"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel
-                      htmlFor="form-name"
-                      className="text-muted-foreground"
-                    >
-                      NAME
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-name"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter your full name"
-                      autoComplete="off"
-                      className="bg-input rounded-sm text-base py-3.5 px-4 h-12"
-                    />
-                    <FieldDescription className="text-border text-[11px]">
-                      3-50 characters, letters only.
-                    </FieldDescription>
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              <Controller
-                name="email"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel
-                      htmlFor="form-email"
-                      className="text-muted-foreground"
-                    >
-                      EMAIL
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-email"
-                      type="email"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="yourname@company.com"
-                      autoComplete="off"
-                      className="bg-input rounded-sm text-base py-3.5 px-4 h-12"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              <Controller
-                name="jobTitle"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel
-                      htmlFor="form-job-title"
-                      className="text-muted-foreground"
-                    >
-                      Job Title (Optional)
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-job-title"
-                      type="text"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="e.g. Project Manager"
-                      autoComplete="off"
-                      className="bg-input rounded-sm text-base py-3.5 px-4 h-12"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              <div className="flex gap-4">
-                <Controller
-                  name="password"
-                  control={control}
-                  render={({ field, fieldState }) => (
-                    <Field data-invalid={fieldState.invalid}>
-                      <FieldLabel
-                        htmlFor="form-password"
-                        className="text-muted-foreground"
-                      >
-                        PASSWORD
-                      </FieldLabel>
-                      <Input
-                        {...field}
-                        id="form-password"
-                        type="password"
-                        aria-invalid={fieldState.invalid}
-                        placeholder="Minimum 8 characters"
-                        autoComplete="off"
-                        className="bg-input rounded-sm text-base py-3.5 px-4 h-12"
-                      />
-                      {fieldState.invalid && (
-                        <FieldError errors={[fieldState.error]} />
-                      )}
-                    </Field>
-                  )}
-                />
-                <Controller
-                  name="confirmPassword"
-                  control={control}
-                  render={({ field, fieldState }) => (
-                    <Field data-invalid={fieldState.invalid}>
-                      <FieldLabel
-                        htmlFor="form-confirm-password"
-                        className="text-muted-foreground"
-                      >
-                        CONFIRM PASSWORD
-                      </FieldLabel>
-                      <Input
-                        {...field}
-                        id="form-confirm-password"
-                        type="password"
-                        aria-invalid={fieldState.invalid}
-                        placeholder="Repeat your password"
-                        autoComplete="off"
-                        className="bg-input rounded-sm text-base py-3.5 px-4 h-12"
-                      />
-                      {fieldState.invalid && (
-                        <FieldError errors={[fieldState.error]} />
-                      )}
-                    </Field>
-                  )}
-                />
-              </div>
-              <Button
-                className="font-semibold h-12 rounded-sm"
-                size="lg"
-                disabled={isPending}
-              >
-                {isPending ? (
-                  <Loader2 className="mr-2 animate-spin" />
-                ) : (
-                  "Create Account"
-                )}
-              </Button>
-            </FieldGroup>
-          </form>
+          <SignupForm />
           <CardFooter className="bg-inherit border-t-0 justify-center">
             <span>
               Already have an account?{" "}
